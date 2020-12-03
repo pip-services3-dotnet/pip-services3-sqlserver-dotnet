9c4d2ac08d641a4f89eb1098761721c303820019
banalna <judas.priest999@gmail.com>
2020-12-03 17:28:03 +0300

    fix links in docs

--- src/Connect/SqlServerConnectionResolver.cs
+++ src/Connect/SqlServerConnectionResolver.cs
@@ -19,20 +19,20 @@ namespace PipServices3.SqlServer.Connect
     /// ### Configuration parameters ###
     /// 
     /// connection(s):
-    /// - discovery_key:               (optional) a key to retrieve the connection from <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a>
+    /// - discovery_key:               (optional) a key to retrieve the connection from <a href="https://pip-services3-dotnet.github.io/pip-services3-components-dotnet/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a>
     /// - host:                        host name or IP address
     /// - port:                        port number (default: 27017)
     /// - database:                    database name
     /// - uri:                         resource URI or connection string with all parameters in it 
     /// 
     /// credential(s):
-    /// - store_key:                   (optional) a key to retrieve the credentials from <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_auth_1_1_i_credential_store.html">ICredentialStore</a>
+    /// - store_key:                   (optional) a key to retrieve the credentials from <a href="https://pip-services3-dotnet.github.io/pip-services3-components-dotnet/interface_pip_services_1_1_components_1_1_auth_1_1_i_credential_store.html">ICredentialStore</a>
     /// - username:                    user name
     /// - password:                    user password
     /// 
     /// ### References ###
     /// 
-    /// - *:discovery:*:*:1.0          (optional) <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a> services
+    /// - *:discovery:*:*:1.0          (optional) <a href="https://pip-services3-dotnet.github.io/pip-services3-components-dotnet/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a> services
     /// - *:credential-store:*:*:1.0   (optional) Credential stores to resolve credentials
     /// </summary>
     public class SqlServerSqlServerResolver: IReferenceable, IConfigurable
